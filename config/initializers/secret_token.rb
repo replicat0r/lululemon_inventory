9a013236b973aaa6091e9ffd53fa7ec35efd4d4f
tyler liu <replicat0r.misc@gmail.com>
2015-01-31 23:43:40 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LululemonInventory::Application.config.secret_key_base = '09618d9b1ecea2f64852cba4dfb98b6ae2f1863eada69a85253d3ea9210fe89093ce6443b1a33b52d48085d83cdfa452d09407fca78ad6c156ee94d40890cbb0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LululemonInventory::Application.config.session_store :cookie_store, key: '_lululemon_inventory_session'
